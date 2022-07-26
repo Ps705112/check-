6e5e6ce4a815d3ce4cf6df91c177ca07d53159db
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2022-07-26 11:19:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-CHANNEL_ID = int(environ.get('CHANNEL_ID'))
+CHANNEL_ID = int(environ.get('CHANNEL_ID')
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
 
 # MongoDB information
